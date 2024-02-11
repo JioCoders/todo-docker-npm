0f7a5f8ecc0d132545b11723ab0f9ef46fe850dc
JioCoders <JioCoders@gmail.com>
2024-02-11 09:22:12 +0530

    wtest03

--- src/app.test.js
+++ src/app.test.js
@@ -3,7 +3,8 @@ const supertest = require("supertest");
 const request = supertest(app);
 
 describe("/test endpoint", () => {
-    it("should return a response", async () => {
+    it("This should return a response", async () => {
+        console.log("Jiocoders should return a response");
         const response = await request.get("/test")
         expect(response.status).toBe(200)
         expect(response.text).toBe("Hello, Jiocoders");
